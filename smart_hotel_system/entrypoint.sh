d7dd294c45d4aae5d8914a5bb23d82872868152c
stephennyansoho@gmail.com <stephennyansoho@gmail.com>
2026-02-15 01:20:27 +0300

    update to production

--- smart_hotel_system/entrypoint.sh
+++ smart_hotel_system/entrypoint.sh
@@ -1,18 +1,10 @@
-# Database configuration
-DATABASE_URL='postgresql://neondb_owner:npg_15KfLqaDolxN@ep-ancient-river-ai1ko3my-pooler.c-4.us-east-1.aws.neon.tech/quickdine_db?sslmode=require&channel_binding=require'
-# Django
-SECRET_KEY=django-insecure-8i_pw1ass4ikn2zsa*^_7*n+96q2
-DEBUG=True
-ALLOWED_HOSTS=*.localhost,*.yourdomain.com,localhost,127.0.0.1
+#!/bin/sh
 
-# Django settings module
-DJANGO_SETTINGS_MODULE=smart_hotel_system.settings
+echo "Running shared migrations..."
+python manage.py migrate_schemas --shared --noinput
 
-# SAFARICOM CREDENTIALS
-CONSUMER_KEY=C4GZYqOQ15gRBmMttmBcWKKbvjbjVjLCcVl8nGIwMYDniUhi
-CONSUMER_SECRET=YuDQYiG33XAjnWQuVwx4plrsD3ZnW3Gqy4WXgeZxgex3IsAY5YCeqeusT5BoddAt
+echo "Running tenant migrations..."
+python manage.py migrate_schemas --tenant --noinput
 
-PASSKEY=bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919
-BUSINESS_SHORTCODE=174379
- 
-
+echo "Starting server..."
+exec "$@"
