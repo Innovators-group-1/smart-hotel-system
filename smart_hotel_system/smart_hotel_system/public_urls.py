42fe1abd2fb452bc1639cb76be6e07ed32331df7
stephennyansoho@gmail.com <stephennyansoho@gmail.com>
2026-03-22 22:57:08 +0300

    resolve urls errors

--- smart_hotel_system/smart_hotel_system/public_urls.py
+++ smart_hotel_system/smart_hotel_system/public_urls.py
@@ -2,10 +2,9 @@
 Public URLs - accessible without tenant context
 """
 from django.urls import path,include
-from . import views
 
 urlpatterns = [
     path('', include('smart_hotel_system.views.home_urls')),
     path('', include('smart_hotel_system.views.admin_urls')),
-    path('health/', views.health_check, name='health'), 
+    path('', include('smart_hotel_system.views.health_check_urls'))
 ]
